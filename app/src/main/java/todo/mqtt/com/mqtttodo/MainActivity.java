bef844d28d50cedf6ef76c6ece0e8b073a8c7528
Emmanuel <emmanuel.colon24@gmail.com>
2017-03-27 10:11:18 -0400

    Initial Commit

---
+++ app/src/main/java/todo/mqtt/com/mqtttodo/MainActivity.java
@@ -0,0 +1,13 @@
+package todo.mqtt.com.mqtttodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
